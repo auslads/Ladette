ca4c6dfc4970dccc8fcb33650278d7ff1e729a51
Ahmadmfk <mastersoffreedom@gmail.com>
2018-11-11 15:35:33 +0400

    initial Commit

---
+++ src/la/pkg10/La10.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package la.pkg10;
+
+/**
+ *
+ * @author b00064740
+ */
+public class La10 {
+public static void fun()
+{
+    
+    System.out.println("Hi");
+    
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       fun();
+    }
+    
+}
